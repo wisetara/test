4d89fb4217e21e3b0cbaf2f9bb607d10b35ba1f1
epicodus <student@epicodus.com>
2014-02-20 17:03:36 -0800

    Completed Italian project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
-var factorial = function(number) {
-        
-  if (number === 0) {
+/*var fibonacci = function(number) {
+   if (number === 1) {
+    return 0; 
+     } else {
     return 1;
-  } else {
-     var result = 1;
-     for (var count = 2; count <= number; count++)
-     result = result * count; 
-     return result;  
-  };
+   };
 };
 
+var fibAdding = function(factor1, factor2) {
+  var result = factor1 + factor2;
+  var resultArray = [];
+  resultArray.push(result);
+  return resultArray;
+};
+
+
+var fibAccess = function(position) {
+  var fibArray = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
+  if (position <= 21) {
+  return fibArray[position - 2] }
+  else {
+    alert('Why the hell do you need to know that matey, argggg!');
+  }
+};*/
+
+var doTheDamnThang = function(position) {
+  if (position < 2)
+    return position;
+  var previous = 0;
+  var current = 1;
+  for (var index = 2; index < position; index++) {
+    var next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
+};
+doTheDamnThang(100)
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,30 @@
-describe("factorial", function() {
-  it ("returns 1 when user enters 0", function() {
-    factorial(0).should.equal(1);
+/*describe("fibonacci", function(number) {
+  it ("returns the first number in the fibonacci sequence", function() {
+    fibonacci(1).should.equal(0);
   });
-
-  it ("multiplies a number by all of the positive integers less than number", function (){
-    factorial(5).should.equal(120);
+  it ("returns the second number in the fibonacci sequence", function() {
+    fibonacci(2).should.equal(1);  
   });
 });
+
+describe("fibAdding", function(factor1, factor2) {
+  it ("adds two numbers and stores the result in an array", function() {
+    fibAdding(1,2).should.eql([3]);
+  });
+});  
+
+describe("fibAccess", function(position) {
+  it ("returns the number in the entered position of the fib sequence", function() {
+    fibAccess(9).should.equal(21);
+  });
+  it ("returns a pirate alert", function() {
+    fibAccess(22).should.equal(alert);
+  });
+});*/
+
+describe("doTheDamnThang", function(position) {
+  it ("returns the value in the selected placeholder in the fibonacci sequence array", function() {
+    doTheDamnThang(9).should.equal(21);
+  });
+});  
 
